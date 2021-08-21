b197f144045b1b94ef52a2c71a1fe7b65aae218c
Najmiddin Mirzoboev <50197492+najeek@users.noreply.github.com>
2021-08-22 00:18:06 +0500

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+//Hidden menu for mobile devises
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+  
